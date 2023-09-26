549c3b018984a27331a7a03898073dbd6bf0ebba
boy-brahmanda <boybrahmanda03@gmail.com>
2023-09-26 10:36:22 +0800

    Finish #1 without coroutines

--- local-data-persistent/LatihanRepository (starter)/kotlin/app/src/main/java/com/dicoding/newsapp/ui/NewsFragment.kt
+++ local-data-persistent/LatihanRepository (starter)/kotlin/app/src/main/java/com/dicoding/newsapp/ui/NewsFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.dicoding.newsapp.data.Result
 import com.dicoding.newsapp.databinding.FragmentNewsBinding
 
 class NewsFragment : Fragment() {
@@ -15,7 +18,9 @@ class NewsFragment : Fragment() {
     private var _binding: FragmentNewsBinding? = null
     private val binding get() = _binding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
         _binding = FragmentNewsBinding.inflate(layoutInflater, container, false)
         return binding?.root
     }
@@ -24,8 +29,48 @@ class NewsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         tabName = arguments?.getString(ARG_TAB)
 
-        val newsAdapter = NewsAdapter()
-        
+        val factory = ViewModelFactory.getInstance(requireActivity())
+        val viewModel: NewsViewModel by viewModels {
+            factory
+        }
+
+        val newsAdapter = NewsAdapter { news ->
+            if (news.isBookmarked) {
+                viewModel.deleteNews(news)
+            } else {
+                viewModel.saveNews(news)
+            }
+        }
+
+        if (tabName == TAB_NEWS) {
+            viewModel.getHeadlineNews().observe(viewLifecycleOwner) { result ->
+                if (result != null) {
+                    when (result) {
+                        is Result.Loading -> {
+                            binding?.progressBar?.visibility = View.VISIBLE
+                        }
+
+                        is Result.Success -> {
+                            binding?.progressBar?.visibility = View.GONE
+                            val newsData = result.data
+                            newsAdapter.submitList(newsData)
+                        }
+
+                        is Result.Error -> {
+                            binding?.progressBar?.visibility = View.GONE
+                            Toast.makeText(context, "Terjadi kesalahan", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+
+            }
+        } else if (tabName == TAB_BOOKMARK) {
+            viewModel.getBookmarkedNews().observe(viewLifecycleOwner) { bookmarkedNews ->
+                binding?.progressBar?.visibility = View.GONE
+                newsAdapter.submitList(bookmarkedNews)
+            }
+        }
+
         binding?.rvNews?.apply {
             layoutManager = LinearLayoutManager(context)
             setHasFixedSize(true)
